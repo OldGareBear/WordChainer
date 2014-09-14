ff1c9d1a291557fccc744d4a62c67754451a28e6
Gary Bethea <betheagary@gmail.com>
2014-09-14 13:33:20 -0400

    Phase II commit

--- hangman.rb
+++ hangman.rb
@@ -6,6 +6,8 @@ class WordChainer
   def initialize(dictionary_file_name)
     dictionary_array = File.readlines(dictionary_file_name).map(&:chomp)
     @dictionary = Set.new(dictionary_array)
+    @current_words = []
+    @all_seen_words = []
   end
   
   def adjacent_words(word)
@@ -23,9 +25,32 @@ class WordChainer
     adjacents
   end
   
-  
   def run(source, target)
+    @current_words << source
+    @all_seen_words << source
+    
+    until @current_words.empty?
+      new_current_words = []
+      
+      @current_words.each do |current_word|
+        adjacent_words = adjacent_words(current_word)
+        
+        adjacent_words.each do |adjacent_word|
+          next if @all_seen_words.include?(adjacent_word)
+          new_current_words << adjacent_word
+          @all_seen_words << adjacent_word
+        end
+      end
+      
+      puts new_current_words
+      @current_words = new_current_words
+    end
+    
+    nil
   end
   
 end
 
+w = WordChainer.new("dictionary.txt")
+
+w.run("fuck", "ruby")
